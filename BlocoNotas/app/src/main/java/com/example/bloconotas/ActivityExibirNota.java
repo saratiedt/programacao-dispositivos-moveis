cce7ed192a3101c27fc30d8cb6b7d8379033c30c
saratiedt <saracristinetiedt@gmail.com>
2021-09-03 15:21:41 -0300

    adicionado modelo nota

--- BlocoNotas/app/src/main/java/com/example/bloconotas/ActivityExibirNota.java
+++ BlocoNotas/app/src/main/java/com/example/bloconotas/ActivityExibirNota.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityExibirNota extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_exibir_nota);
     }
-
-    public void cadastrarNota(View v) {}
-
 }
