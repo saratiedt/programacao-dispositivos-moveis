daf62807735f3e64e1f2614669689ff73b789e1b
saratiedt <saracristinetiedt@gmail.com>
2021-09-08 16:52:34 -0300

    tirar foto

---
+++ TirarFoto/app/src/main/java/com/example/tirarfoto/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.tirarfoto;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imagem;
+    Button tirarFoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imagem = findViewById(R.id.imageView);
+        tirarFoto = findViewById(R.id.button);
+
+        tirarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, 1);
+                }else{
+                    Toast.makeText(MainActivity.this, "Não possui superte a esta ação", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            Bundle bundle = data.getExtras();
+            Bitmap fotoFinal = (Bitmap) bundle.get("data");
+            imagem.setImageBitmap(fotoFinal);
+        }
+    }
+}
